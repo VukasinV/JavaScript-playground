1558c670534818cca8f40b6ed052595da30c5993
Vukasin Vranic <Vukasin.Vranic@StadaITSolutions.com>
2019-10-02 10:58:44 +0200

    Initital commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// selection sort
+// Start from first element, find smallest element in the rest of array and
+// swap it with current element
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[min] > arr[j]) min = j;
+    }
+
+    if (i !== min) {
+      let temp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = temp;
+    }
+  }
+  return arr;
+}
